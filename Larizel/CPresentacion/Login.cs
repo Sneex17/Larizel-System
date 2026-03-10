0e5943b5f740cbf24d89f5608645d928c897a861
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-03-10 09:00:00 -0400

    Arreglos de cambios

--- Larizel/CPresentacion/Login.cs
+++ Larizel/CPresentacion/Login.cs
@@ -1,8 +1,8 @@
 namespace CPresentacion
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
--- Larizel/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
+++ Larizel/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff0c22571ce78283148b2d0e2e8789e67f46372")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0f6d416b5d464350f919d5715277d106dffe1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
